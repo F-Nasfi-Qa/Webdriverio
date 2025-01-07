efb5c262e4b65331962f6e3d5f3173808238debd
FaresNASFI <f.nasfi@vocalcom.com>
2025-01-07 08:46:04 +0100

    commit_alerts&Popups.js

--- features/step-definitions/alerts&Popups.js
+++ features/step-definitions/alerts&Popups.js
@@ -1,4 +1,5 @@
 import { Given, When } from "@wdio/cucumber-framework";
+import { browser } from "@wdio/globals";
 
 /**
  * Alerts and popups feature
@@ -14,21 +15,21 @@ When("Alerts and popups interactions", async () => {
 
   // 1.Open the Js alert
   await $("=Click for JS Alert").click();
-  //if (await browser.isAlertOpen()) {
-    //await browser.acceptAlert();
-  //}
+  if (await browser.isAlertOpen()) {
+    await browser.acceptAlert();
+  }
 
-  // 2.Open the Js confirm
-  await $("=Click for JS Confirm").click();
-  // // if (await browser.isAlertOpen()) {
-  // //   await browser.dismissAlert();
-  // // }
+  // // 2.Open the Js confirm
+  // await $("=Click for JS Confirm").click();
+  // if (browser.isAlertOpen()) {
+  //   await browser.dismissAlert();
+  // }
 
-  // 3.Open the Js prompt
-  await $("=Click for JS Prompt").click();
-  // // if (await browser.isAlertOpen()) {
-  // //   await browser.dismissAlert();
-  // // }
+  // // 3.Open the Js prompt
+  // await $("=Click for JS Prompt").click();
+  // if (browser.isAlertOpen()) {
+  //   await browser.dismissAlert();
+  // }
 
   await browser.debug();
 });
