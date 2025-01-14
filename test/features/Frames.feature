dfad665be9756487ffca57e787fc553b4ad2f40d
FaresNASFI <f.nasfi@vocalcom.com>
2025-01-14 13:51:01 +0100

    frames.js creation (1)

---
+++ test/features/Frames.feature
@@ -0,0 +1,9 @@
+Feature: Frames feature
+
+  @Frames
+  Scenario Outline: Frames scenario
+    Given Frames page is opened
+    When Frames interactions
+
+    Examples:
+      | testId |
---
+++ test/step-definitions/frames.js
@@ -0,0 +1,9 @@
+import { Given, When } from "@wdio/cucumber-framework";
+
+Given(`Frames page is opened`, () => {
+  // [Given] Sets up the initial state of the system.
+});
+
+When(`Frames interactions`, () => {
+  // [When] Describes the action or event that triggers the scenario.
+});
